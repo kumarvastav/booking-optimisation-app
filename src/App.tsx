ae9f9813e0e27315587adff94ddc00507b6e5b3d
Kumar Vastav <srivastav>
2023-11-04 19:13:03 +0530

    Modified the logic to allocate rooms accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 const GUESTS = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
 
+
 function calculateOccupancy(premiumRooms: any, economyRooms: any) {
   const sortedGuests = [...GUESTS].sort((a, b) => b - a);
   let premiumOccupancy = 0;
@@ -11,20 +12,30 @@ function calculateOccupancy(premiumRooms: any, economyRooms: any) {
   let economyRevenue = 0;
 
   for (const guest of sortedGuests) {
-    if (guest >= 100) {
-      continue;
-    }
-
-    if (premiumRooms > 0) {
-      premiumOccupancy++;
-      premiumRevenue += guest;
-      premiumRooms--;
-    } else if (economyRooms > 0) {
-      economyOccupancy++;
-      economyRevenue += guest;
-      economyRooms--;
+    if (guest < 100) {
+        if (economyRooms > 0) {
+          economyOccupancy++;
+          economyRevenue += guest;
+          economyRooms--;
+        } else if (premiumRooms > 0) {
+          premiumOccupancy++;
+          premiumRevenue += guest;
+          premiumRooms--;
+        } else {
+          break;
+        }
     } else {
-      break;
+        if (premiumRooms > 0) {
+            premiumOccupancy++;
+            premiumRevenue += guest;
+            premiumRooms--;
+        } else if (economyRooms > 0) {
+           economyOccupancy++;
+           economyRevenue += guest;
+           economyRooms--;
+           } else {
+                break;
+           }
     }
   }
 
