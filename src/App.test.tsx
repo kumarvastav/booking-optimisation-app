5b6d1b2acd41d24283e49d5bc8cc13c3f096dc72
Kumar Vastav <srivastav>
2023-11-04 20:07:42 +0530

    Added some basic unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders calculate occupancy link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Calculate Occupancy');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders premium rooms label', () => {
+  render(<App />);
+  const labelElement = screen.getByText('Premium Rooms:');
+  expect(labelElement).toBeInTheDocument();
+});
+
+test('renders economy rooms label', () => {
+  render(<App />);
+  const labelElement = screen.getByText('Economy Rooms:');
+  expect(labelElement).toBeInTheDocument();
+});
+
+test('renders default input values', () => {
+  render(<App />);
+  const inputElements = screen.getAllByRole('spinbutton');
+  expect(inputElements[0]).toHaveValue(0);
+  expect(inputElements[1]).toHaveValue(0);
 });
